3ab38a85ec12c185788bff184231a43fdacd0815
John Shultz <john.shultz@mymail.champlain.edu>
2020-02-07 12:20:59 -0500

    Adding Skeletons of the two files needed to complete the week 4 lab.

---
+++ Week 4/Week 4 Lab/medical.py
@@ -0,0 +1,8 @@
+# Skeleton of medical.py file that creates the classes with no arguments.
+
+class Patient:
+	pass
+
+
+class Procedure:
+	pass
---
+++ Week 4/Week 4 Lab/week4.py
@@ -0,0 +1,3 @@
+# A skeleton of the week4.py CLI interface for interacting with the objects in medical.py
+from medical import patient, procedure
+
