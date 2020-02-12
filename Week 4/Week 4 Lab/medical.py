71b4ea177feda3a1b67fc7cea6526ca6d11a384d
John Shultz <john.shultz@mymail.champlain.edu>
2020-02-11 20:43:58 -0500

    Added final version of medical.py and week4.py. __str__ function of Patient may need some revision in future.

--- Week 4/Week 4 Lab/medical.py
+++ Week 4/Week 4 Lab/medical.py
@@ -1,8 +1,130 @@
-# Skeleton of medical.py file that creates the classes with no arguments.
+import pickle
+
 
 class Patient:
-	pass
+	# Private class variable ID. Prefixed with an underscore as
+	_next_id = 1
+	_all_patients = dict()
+
+	def __str__(self):
+
+		# Workaround to handle the way that f strings handle objects in lists.
+		print_procedures = []
+		for procedure in self.procedures:
+			print_procedures.append(str(procedure))
+
+		return f'Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nHeight: {self.height}\n' \
+			f'Weight: {self.weight}\nUpcoming Procedures: {print_procedures}\nContacts: {self.contact_list}'
+
+
+	def __init__(self, name, age, gender, height, weight, contact_list=dict()):
+		"""Initialize a new Patient
+
+		:param name: (str) The name of the patient.
+		:param age: (int) the age of the patient
+		:param gender: (str) The gender of the patient
+		:param height: (str) The height of the patient ex. 5'8"
+		:param weight: (int) The patient's weight in lbs
+		:param contact_list: (dict) A dictionary of contact numbers. ex. {'Cell': "123-456-7890", "Mom": "555-555-5555"}
+		"""
+
+		# Set the patient's id to the next ID, and then increment the value of next id.
+		self._id = Patient._next_id
+		Patient._next_id += 1
+
+		# Add the new patient to the dictionary of patients, indexed by it's ID
+		Patient._all_patients[self._id] = self
+
+		# Assign the values of other patient properties to instance variables.
+		self.name = name
+		self.age = age
+		self.gender = gender
+		self.height = height
+		self.weight = weight
+		self.contact_list = contact_list
+
+		# Create an empty list to store procedures
+		self.procedures = []
+
+	# Appends the procedure to the procedure list.
+	def add_procedure(self, procedure):
+		"""Adds a procedure to the patient's procedure list.
+
+		:param procedure: (Procedure) The procedure to be added
+		:return: Nothing. Modifies instance variable procedures.
+		"""
+		self.procedures.append(procedure)
+
+	@staticmethod
+	def get_patient(patient_id):
+		"""Returns a patient object with the given ID number.
+
+		:param patient_id: (int) The requested Patient ID
+		:return: If patient found, returns the Patient object. Otherwise, returns nothing.
+		"""
+
+		# Tries to return the patient with that given ID. Catches exception if there is no patient, returns None.
+		try:
+			return Patient._all_patients[patient_id]
+		except KeyError:
+			return None
+
+	@staticmethod
+	def delete_patient(patient_id):
+		"""Deletes a patient from the list of all patients
+
+		:param patient_id: (int) The ID of the patients that should be delted
+		:return: Nothing. Modifies class variable _all_patients
+		"""
+
+		# Try to delete the given ID from the list of all patients. If it exists, delete it. If not, do nothing.
+		try:
+			del Patient._all_patients[patient_id]
+		except KeyError:
+			pass
+
+	@staticmethod
+	def save_patients(file_name):
+		"""Create a pickle file containing the _all_patients dictionary
+
+		:param file_name: (str) The desired file path and name for the pickle file. No file extension should be included
+		:return: Nothing. Saves a pickle file to the user's drive at the path listed in file_name
+		"""
+		pickle.dump(Patient._all_patients, open(f'{file_name}.p', 'wb'))
+
+	@staticmethod
+	def load_patients(file_name):
+		"""Load a pickle file containing an _all_patients dictionary to the Patient._all_parents dictionary
+
+		:param file_name: (str) The desired file path and name for the pickle file. No file extension should be included
+		:return: Nothing. Either loads file or prints file not found.
+		"""
+		try:
+			Patient._all_patients = pickle.load(open(f'{file_name}.p', 'rb'))
+		except FileNotFoundError:
+			print("No file with that name found!")
 
 
 class Procedure:
-	pass
+	_next_id = 1
+
+	def __str__(self):
+		return f'On {self.date} {self.doctor} will perform {self.name} at {self.location}'
+
+	def __init__(self, name, doctor, location, date):
+		"""Initializes a new Procedure
+
+		:param name: (str) The name of the procedure. ex. Yearly Checkup
+		:param doctor: (str) The name of the medical professional performing the procedure
+		:param location: (str) The location the procedure will be performed at. ex. 123 Fake Street ex2. Main Office
+		:param date: (str) The date the procedure will be performed ex. 1/1/1970
+		"""
+
+		# Set the ID of the procedure to the next ID.
+		self._id = Procedure._next_id
+
+		# Assign other values based on user input
+		self.name = name
+		self.doctor = doctor
+		self.location = location
+		self.date = date
--- Week 4/Week 4 Lab/week4.py
+++ Week 4/Week 4 Lab/week4.py
@@ -1,3 +1,142 @@
 # A skeleton of the week4.py CLI interface for interacting with the objects in medical.py
-from medical import patient, procedure
+from medical import Patient, Procedure
 
+print("Hi! Welcome to the patient management tool! Before we can begin, we need to load the patient_info.")
+file_name = str(input("Plese enter in the full file path and name without extension where we can find this file: "))
+
+Patient.load_patients(file_name)
+
+while True:
+	print("--Main Menu--")
+	print("1. Look up a Patient by ID")
+	print("2. Add a new Patient")
+	print("3. Quit")
+
+	# Take the users main menu option. Except any non number answers
+	try:
+		user_choice = int(input("Please enter in the number you would like to select: "))
+	except ValueError:
+		print("That is not a valid number!")
+
+	if user_choice == 1:
+		patient_id = int(input("Please enter in the patient ID number: "))
+		current_patient = Patient.get_patient(patient_id)
+
+		# If there was no patient with that ID, current patient will store None and evaluate as false.
+		if current_patient:
+
+			# Print all the patient info, as well as a list of patient options
+			print(current_patient)
+			print("1. Change Name")
+			print("2. Change Age")
+			print("3. Change Gender")
+			print("4. Change Height")
+			print("5. Change Weight")
+			print("6. Add or Modify Contact")
+			print("7. Remove Contact")
+			print("8. Add Procedure")
+			print("9. Delete Patient")
+			print("10. Do Nothing")
+
+			# Try to take in the user's choice and perform the action. Catch non-number choices and except them.
+			try:
+				patient_choice = int(input("Please enter in which option you would like to perform: "))
+
+				# Change Name
+				if patient_choice == 1:
+					current_patient.name = str(input("Please enter in the new name for the patient: "))
+
+				# Change Age
+				elif patient_choice == 2:
+					current_patient.age = int(input("Please enter in the new age for the patient: "))
+
+				# Change gender
+				elif patient_choice == 3:
+					current_patient.gender = str(input("Please enter in the new gender for the patient: "))
+
+				# Change height
+				elif patient_choice == 4:
+					current_patient.height = str(input("Please enter the new height for the patient: "))
+
+				# Change Weight
+				elif patient_choice == 5:
+					current_patient.weight = int(input("Please enter the new weight in lbs for the patient: "))
+
+				# Add or Modify a contact number
+				elif patient_choice == 6:
+					contact = str(input("Please enter in the label for the new contact or contact to be modified"))
+					number = str(input("Please enter in the new number"))
+					current_patient.contact_list[contact] = number
+
+				# Remove Contact
+				elif patient_choice == 7:
+
+					# Print out a list of all currently saved contacts
+					for contact in current_patient.contact_list.keys():
+						print(contact)
+
+					# Delete the contact of the user's choice.
+					contact = str(input("Which of the above contacts would you like to delete?"))
+					del current_patient.contact_list[contact]
+
+				# Add procedure
+				elif patient_choice == 8:
+					procedure_name = str(input("Please enter the name of the procedure: "))
+					procedure_doctor = str(input("Please enter the name of the doctor performing the procedure: "))
+					procedure_location = str(input("Please enter where this procedure will be performed: "))
+					procedure_date = str(input("Please enter the date this procedure will be performed: "))
+					new_procedure = Procedure(procedure_name, procedure_doctor, procedure_location, procedure_date)
+					current_patient.add_procedure(new_procedure)
+
+				# Delete Patient
+				elif patient_choice == 9:
+
+					# Check to make sure user is sure, if they are, call delete patient function.
+					accident_check = str(input("Are you sure (Y/N): "))
+					if accident_check.upper() == "Y" or accident_check.upper() == "YES":
+						Patient.delete_patient(patient_id)
+
+				# Do Nothing
+				elif patient_choice == 10:
+					pass
+
+				# If the user makes a non numbered choice, send them to main menu
+				else:
+					print("That was not a valid choice! Returning to main menu.")
+
+			except ValueError:
+				print("That is not a valid number! Returning to main menu.")
+		else:
+			print("There is no patient with that ID.")
+
+	elif user_choice == 2:
+		name = str(input("Enter the name of the patient: "))
+		age = int(input("Enter in the age of the patient: "))
+		gender = str(input("Enter in the gender of the patient: "))
+		height = str(input("Enter in the height of the patient: "))
+		weight = int(input("Enter in the weight of the patient in lbs: "))
+
+		contact_list = {}
+		print("You may optionally enter in patient contact numbers.")
+
+		# Allow for users to enter in as many contacts as they desire with a null terminated loop.
+		while True:
+			contact = str(input("Please enter either label for this contact, or nothing to finish entering contacts: "))
+
+			# Use string truthiness. If the contact is empty, break the loop. Otherwise, prompt for the number.
+			if contact:
+				number = str(input("Please enter the phone number associated with this label"))
+				contact_list[contact] = number
+			else:
+				break
+
+		# Create the new patient.
+		Patient(name, age, gender, height, weight, contact_list)
+
+	elif user_choice == 3:
+		print("Before you quit we need to save a copy of the patients list.")
+		file_name = str(input("Enter in the full path and file name without extension you would like to use: "))
+		Patient.save_patients(file_name)
+		break
+	else:
+		print("That number was not associated with any possible choice!")
