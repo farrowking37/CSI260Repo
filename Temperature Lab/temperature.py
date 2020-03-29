0f149db720203029192b2c0f007c303ea87fa181
John Shultz <john.shultz@mymail.champlain.edu>
2020-03-29 18:01:59 -0400

    Added certificate of authenticity.

--- Temperature Lab/temperature.py
+++ Temperature Lab/temperature.py
@@ -1,3 +1,22 @@
+"""Impliments a class called temperature that is able to store a temperature value in celsius and convert it dynamically
+to the other two common temperature scales.
+
+Author: John Shultz
+Class: CSI-260-03
+Assignment: Week 11 Lab
+Due Date: April 1st, 2020 11:00 AM
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
 """Classes for working with Temperatures."""
 import re
 
